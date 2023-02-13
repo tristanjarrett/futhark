398e226c976226ca85e4a6ce0a2ed4aba0b41173
Tristan Jarrett <tristan.jarrett@outlook.com>
2023-02-13 21:35:31 +0000

    Initial Commit

---
+++ futhark/futharkApp.swift
@@ -0,0 +1,17 @@
+//
+//  futharkApp.swift
+//  futhark
+//
+//  Created by Tristan Jarrett on 13/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct futharkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
